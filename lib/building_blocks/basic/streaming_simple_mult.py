25d7cde9d43d0c627b8ebe1fb67b8799fae7c64e
skr <krsheshu@gmail.com>
2018-09-08 00:40:05 +0200

    shakti: added streaming building blocks

--- lib/building_blocks/basic/streaming_simple_mult.py
+++ lib/building_blocks/basic/streaming_simple_mult.py
@@ -1,39 +1,31 @@
 import myhdl
 from avalon_buses import AvalonST_SNK, AvalonST_SRC
-from streaming_ip_a import streaming_ip_a_2xinp
+from streaming_ip_a_2xinp import streaming_ip_a_2xinp
 
-class SimpleAdderPars():
+class StreamingSimpleMultPars():
   def __init__(self):
-    """ simple add pars """
-    self.IN1_SYMBOL_WIDTH=32
-    self.IN2_SYMBOL_WIDTH=32
-    self.OP_SYMBOL_WIDTH=32
+    """ streaming simple mult pars """
+    self.SNK0_SYMBOL_WIDTH=32
+    self.SNK1_SYMBOL_WIDTH=32
+    self.SRC_SYMBOL_WIDTH=32+32
   def __call__(self, pars):
-    self.IN1_SYMBOL_WIDTH   = pars.IN1_SYMBOL_WIDTH
-    self.IN2_SYMBOL_WIDTH   = pars.IN2_SYMBOL_WIDTH
-    self.OP_SYMBOL_WIDTH    = pars.OP_SYMBOL_WIDTH
+    self.SNK0_SYMBOL_WIDTH   = pars.SNK0_SYMBOL_WIDTH
+    self.SNK1_SYMBOL_WIDTH   = pars.SNK1_SYMBOL_WIDTH
+    self.SRC_SYMBOL_WIDTH    = pars.SNK0_SYMBOL_WIDTH + pars.SNK1_SYMBOL_WIDTH
 
-class SimpleAdderIo():
-  """ simple add Interface Signals """
-  def __init__(self,pars):
-    self.in1_i  = Signal(intbv(0)[pars.IN1_SYMBOL_WIDTH:])
-    self.in2_i  = Signal(intbv(0)[pars.IN2_SYMBOL_WIDTH:])
-    self.op_o   = Signal(intbv(0)[pars.OP_SYMBOL_WIDTH:])
-
-
-class SimpleAdder():
-  """ SimpleAdder Top """
+class StreamingSimpleMult():
+  """ StreamingSimpleMult Top """
   def __init__(self):
     pass
-
-  def block_connect(pars, reset, clk, av_snk0, av_snk1, av_src, data_enable_o):
-    """ simple adder block"""
+  
+  def block_connect(pars, reset, clk, av_snk0, av_snk1, av_src):
+    """ simple mult block"""
 
   data_enable_o=Signal(bool(0))
 
-  av_snk0_if = AvalonST_SNK(len(av_snk0.data_i))
-  av_snk1_if = AvalonST_SNK(len(av_snk1.data_i))
-  av_src_if = AvalonST_SRC(len(av_src.data_o))
+  av_snk0_if = AvalonST_SNK(pars.SNK0_SYMBOL_WIDTH)
+  av_snk1_if = AvalonST_SNK(pars.SNK1_SYMBOL_WIDTH)
+  av_src_if = AvalonST_SRC(pars.SRC_SYMBOL_WIDTH)
 
 
   @always_comb
@@ -50,8 +42,8 @@ class SimpleAdder():
   streaming_ip_a_2xinp_inst = streaming_ip_a_2xinp(reset, clk, av_snk0_if, av_snk1_if, av_src_if, data_enable_o )
 
   @always(clk.posedge, reset.posedge)
-  def simple_adder():
-    """ simple adder """
+  def simple_mult():
+    """ streaming simple mult """
     if reset==1:
       av_src.startofpacket_o.next = 0
       av_src.endofpacket_o.next = 0
@@ -59,7 +51,7 @@ class SimpleAdder():
       av_src.channel_o.next = 0
     elif data_enable_o == 1:
       av_src.startofpacket_o.next = av_snk0.startofpacket_i and av_snk1.startofpacket_i 
-      av_src.data_o.next = av_snk0.data_i + av_snk1.data_i
+      av_src.data_o.next = av_snk0.data_i * av_snk1.data_i
       av_src.endofpacket_o.next = av_snk0.endofpacket_i and av_snk1.endofpacket_i
       av_src.channel_o.next = av_snk0.channel_i and av_snk1.channel_i
    
---
+++ lib/building_blocks/basic/streaming_simple_square.py
@@ -0,0 +1,51 @@
+import myhdl
+from avalon_buses import AvalonST_SNK, AvalonST_SRC
+from streaming_ip_a import streaming_ip_a
+
+class StreamingSimpleSquarePars():
+  def __init__(self):
+    """ simple square pars """
+    self.SNK_SYMBOL_WIDTH=32
+    self.SRC_SYMBOL_WIDTH=32+32
+  def __call__(self, pars):
+    self.SNK_SYMBOL_WIDTH   = pars.SNK_SYMBOL_WIDTH
+    self.SRC_SYMBOL_WIDTH   = pars.SRC_SYMBOL_WIDTH
+
+class StreamingSimpleSquare():
+  """ StreamingSimpleSquare Top """
+  def __init__(self):
+    pass
+
+  def block_connect(pars, reset, clk, av_snk, av_src):
+    """ streaming_simple square block"""
+
+  data_enable_o=Signal(bool(0))
+
+  av_snk_if = AvalonST_SNK(pars.SNK_SYMBOL_WIDTH)
+  av_src_if = AvalonST_SRC(pars.SRC_SYMBOL_WIDTH)
+
+
+  @always_comb
+  def signal_assignments():
+    av_snk_if.valid_i.next=av_snk.valid_i
+    av_snk.ready_o.next=av_snk_if.ready_o
+    
+    av_src.valid_o.next=av_src_if.valid_o
+    av_src_if.ready_i.next=av_src.ready_i
+    
+
+  streaming_ip_a_inst = streaming_ip_a(reset, clk, av_snk_if, av_src_if, data_enable_o )
+
+  @always(clk.posedge, reset.posedge)
+  def streaming_simple_square():
+    """ streaming_simple square """
+    if reset==1:
+      av_src.startofpacket_o.next = 0
+      av_src.endofpacket_o.next = 0
+      av_src.data_o.next = 0
+      av_src.channel_o.next = 0
+    elif data_enable_o == 1:
+      av_src.startofpacket_o.next = av_snk.startofpacket_i  
+      av_src.data_o.next = av_snk.data_i * av_snk.data_i
+      av_src.endofpacket_o.next = av_snk.endofpacket_i 
+      av_src.channel_o.next = av_snk.channel_i
