635845999242db848416fa1ec13381818f43d0e8
skr <skr@smartray.com>
2018-09-08 11:32:28 +0200

    shakti: added clk driver

---
+++ lib/smr_pkg/smr_clock_driver/myhdl/clk_driver.py
@@ -0,0 +1,25 @@
+from myhdl import Signal, delay, instance, always, always_comb
+
+def clk_driver(elapsed_time,clk, period=20):
+
+  cycle_count=Signal(0)
+  lowTime = int(period/2)
+  highTime = period - lowTime
+
+  @instance
+  def driveClk():
+    while True:
+      yield delay(lowTime)
+      clk.next = 1
+      yield delay(highTime)
+      clk.next = 0
+
+  @always(clk.negedge)
+  def num_clocks():
+    cycle_count.next = cycle_count+1
+
+  @always_comb
+  def find_elapsed_time():
+    elapsed_time.next=period*(cycle_count)
+
+  return driveClk, num_clocks, find_elapsed_time
---
+++ lib/smr_pkg/smr_clock_driver/myhdl/rst_driver.py
@@ -0,0 +1,20 @@
+from myhdl import always_comb
+
+def rst_driver(elapsed_time,reset_duration, reset_high, reset_low):
+
+  @always_comb
+  def reset_high_function():
+    if elapsed_time < reset_duration:
+      reset_high.next = 1
+    else:
+      reset_high.next = 0
+
+  @always_comb
+  def reset_low_function():
+    if elapsed_time < reset_duration:
+      reset_low.next = 0
+    else:
+      reset_low.next = 1
+
+  return reset_high_function, reset_low_function
+
