338e3d8c12a86afe912884d509c64a88ec058434
skr <skr@smartray.com>
2019-04-04 18:54:19 +0200

    naming issue sandbox code updated

--- sandbox/naming_issue.py
+++ sandbox/naming_issue.py
@@ -4,33 +4,48 @@ from myhdl import Signal, intbv, toVerilog, instances, always_comb, block
 
 class stimulus():
   def __init__(self):
-    self.data = Signal(bool(0))
+    self.data = Signal(intbv(0)[32:])
     self.valid  = Signal(bool(0))
           
   def __call__(self,data,valid):
-     self.data = Signal(bool(data))
+     self.data = Signal(intbv(data)[32:])
      self.valid= Signal(bool(valid))
 
-def block_connect(inpa):
-  shift=Signal(bool(0))  
+class pipe():
+  def __init__(self):
+    self.pipeInp=stimulus()
+  def __call__(self,stim):
+    self.pipeInp=stim
+
+class mainblk():
+  def block_connect(self,inpa):
+    shift2=Signal(bool(0))  
+    
+    @always_comb
+    def shift2_process():
+      shift2.next = 1 if  (inpa.valid == 1) else 0
   
-  @always_comb
-  def shift2_process():
-    shift.next = 1 if  (inpa.valid == 1) else 0
-  pass
+    return instances()
 
-def test_top(inpa):
-  shift=Signal(bool(0))  
+class testblk():
+  def test_top(self,inpa):
+    shift1=Signal(bool(0))  
+    
+    @always_comb
+    def shift1_process():
+      shift1.next = 1 if  (inpa.valid == 1) else 0
+   
+    testMain=mainblk()
+    inpConnect=(testMain.block_connect(inpa))
   
-  @always_comb
-  def shift_process():
-    shift.next = 1 if  (inpa.valid == 1) else 0
-
-  inpConnect=block_connect(inpa)
-
-  return instances()
+    return instances()
 
 if __name__ == '__main__':
-  inpa=stimulus()
-  inpa(1,1)
-  toVerilog(test_top,inpa)
+  stim=stimulus()
+  stim(100,1)
+  pipe_I=pipe()
+  pipe_I(stim)
+  testMod=testblk()
+  filename= "lr_top"
+  toVerilog.name= filename 
+  toVerilog(testMod.test_top,pipe_I.pipeInp)
