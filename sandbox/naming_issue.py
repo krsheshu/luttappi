57a2899dfc0954aab5691cebb3a34e47d3509124
skr <krsheshu@gmail.com>
2019-04-05 08:32:00 +0200

    updated naming issue

--- sandbox/naming_issue.py
+++ sandbox/naming_issue.py
@@ -1,34 +1,30 @@
 #!/usr/bin/python3 
 
-from myhdl import Signal, intbv, toVerilog, instances, always_comb, block
+from myhdl import Signal, intbv, toVerilog, instances, always_comb
+
+def conditional_wire_assign(dout, condition, din1, din2):
+  @always_comb
+  def conditional_wire_assign_process():
+    dout.next = din1 if (condition == 1) else din2
+  return instances() 
 
 class stimulus():
   def __init__(self):
     self.data = Signal(intbv(0)[32:])
     self.valid  = Signal(bool(0))
           
-  def __call__(self,data,valid):
-     self.data = Signal(intbv(data)[32:])
-     self.valid= Signal(bool(valid))
-
-class pipe():
-  def __init__(self):
-    self.pipeInp=stimulus()
-  def __call__(self,stim):
-    self.pipeInp=stim
-
 class mainblk():
-  def block_connect(self,inpa):
-    shift2=Signal(bool(0))  
+  def block_connect(self,reset,clk,inpa):
+    shift2=Signal(bool(1)) 
+    op=stimulus() 
     
-    @always_comb
-    def shift2_process():
-      shift2.next = 1 if  (inpa.valid == 1) else 0
+    valid=(conditional_wire_assign(op.valid, shift2, inpa.valid, 0)) 
+    data=(conditional_wire_assign(op.data, shift2, inpa.data, 0)) 
   
     return instances()
 
 class testblk():
-  def test_top(self,inpa):
+  def test_top(self,reset,clk,inpa):
     shift1=Signal(bool(0))  
     
     @always_comb
@@ -36,16 +32,14 @@ class testblk():
       shift1.next = 1 if  (inpa.valid == 1) else 0
    
     testMain=mainblk()
-    inpConnect=(testMain.block_connect(inpa))
+    inpConnect=(testMain.block_connect(reset, clk, inpa))
   
     return instances()
 
-if __name__ == '__main__':
+if __name__ == "__main__": 
+  reset = Signal(bool(0))
+  clk = Signal(bool(0))
   stim=stimulus()
-  stim(100,1)
-  pipe_I=pipe()
-  pipe_I(stim)
   testMod=testblk()
-  filename= "lr_top"
-  toVerilog.name= filename 
-  toVerilog(testMod.test_top,pipe_I.pipeInp)
+  
+  toVerilog(testMod.test_top,reset,clk,stim)
